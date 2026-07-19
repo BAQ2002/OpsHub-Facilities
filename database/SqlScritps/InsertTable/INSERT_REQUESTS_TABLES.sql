e1a266f23974c3e8906921b35a5511d5700ab578
BAQ2002 <antoniobaqueirocosta@gmail.com>
2026-07-19 20:43:10 -0300

    Alteração no insert para considerar o SERVICE_FIELD_VALUE

--- database/SqlScritps/InsertTable/INSERT_REQUESTS_TABLES.sql
+++ database/SqlScritps/InsertTable/INSERT_REQUESTS_TABLES.sql
@@ -4,9 +4,10 @@ INSERT INTO REQUEST_TYPE (name) VALUES
     ('Atividade de Pátio')
 ON CONFLICT (name) DO NOTHING;
 
-INSERT INTO REQUEST_STATUS (description) VALUES
-    ('Aberto'),
-    ('Programada'),
-    ('Em andamento'),
-    ('Concluída')
+INSERT INTO REQUEST_STATUS (ID, description) VALUES
+    (1,'Em aberto'),
+    (2,'Programada'),
+    (3,'Em andamento'),
+    (4,'Concluída'),
+    (5,'Cancelada'),
 ON CONFLICT (description) DO NOTHING;
