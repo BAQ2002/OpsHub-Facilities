600196cb0f42495f40e215664903a14502cf42f0
BAQ2002 <antoniobaqueirocosta@gmail.com>
2026-07-21 06:41:24 -0300

    alterações INSERT

--- database/SqlScritps/InsertTable/INSERT_REQUESTS_TABLES.sql
+++ database/SqlScritps/InsertTable/INSERT_REQUESTS_TABLES.sql
@@ -1,13 +1,13 @@
 
-INSERT INTO REQUEST_TYPE (name) VALUES
-    ('Chamado'),
-    ('Atividade de Pátio')
-ON CONFLICT (name) DO NOTHING;
+INSERT INTO REQUEST_TYPE (ID, name) VALUES
+    (1, 'Chamado'),
+    (2, 'Atividade de Pátio')
+ON CONFLICT (ID) DO NOTHING;
 
 INSERT INTO REQUEST_STATUS (ID, description) VALUES
     (1,'Em aberto'),
     (2,'Programada'),
     (3,'Em andamento'),
     (4,'Concluída'),
-    (5,'Cancelada'),
-ON CONFLICT (description) DO NOTHING;
+    (5,'Cancelada')
+ON CONFLICT (ID) DO NOTHING;
