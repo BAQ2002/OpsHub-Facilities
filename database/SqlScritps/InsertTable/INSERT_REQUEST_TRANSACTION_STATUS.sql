c7c4ee0da554cc5ed02593d472b91ecdc8ea14c3
ws-e-abac <antonio.baqueiro@wilsonsons.com.br>
2026-07-25 19:44:12 -0300

    Alterção em INSERTS

--- database/SqlScritps/InsertTable/INSERT_REQUEST_TRANSACTION_STATUS.sql
+++ database/SqlScritps/InsertTable/INSERT_REQUEST_TRANSACTION_STATUS.sql
@@ -1,8 +1,7 @@
-INSERT INTO TRANSACTION_STATUS (ID, name) VALUES
+INSERT INTO REQUEST_TRANSACTION_STATUS (ID, name) VALUES
     (1, 'Solicitada'),
     (2, 'Aprovada'),
     (3, 'Retornada'),
-    (4, 'Reprovada'),
-    (5, 'Cancelada')
+    (4, 'Cancelada')
 ON CONFLICT (ID) DO NOTHING;
 
