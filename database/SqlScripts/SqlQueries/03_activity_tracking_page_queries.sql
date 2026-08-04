fd5700a08d11df6a9d3700e2353e394b1c63fcf9
Antonio Baqueiro <132956745+BAQ2002@users.noreply.github.com>
2026-08-04 10:58:01 -0300

    chore(database): classify attachment fields as media

--- database/SqlScripts/SqlQueries/03_activity_tracking_page_queries.sql
+++ database/SqlScripts/SqlQueries/03_activity_tracking_page_queries.sql
@@ -1,6 +1,6 @@
 -- ============================================================
 -- OpsHub Facilities - Acompanhamento de atividades page queries
--- Base schema: SqlScritps/CreateTables/CREATE_ALL_TABLES.sql
+-- Base schema: SqlScripts/CreateTables/CREATE_ALL_TABLES.sql
 -- Objetivo: substituir categoryData, statusData, monthlyData e summaryCards.
 -- ============================================================
 
